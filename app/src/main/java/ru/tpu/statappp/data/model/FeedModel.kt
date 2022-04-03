bac9fd2eb0fd21e83668b371491479aec3eba925
salboro <salboro121@gmail.com>
2022-04-03 22:09:42 +0700

    Fix

--- app/src/main/java/ru/tpu/statappp/data/model/FeedModel.kt
+++ app/src/main/java/ru/tpu/statappp/data/model/FeedModel.kt
@@ -5,7 +5,16 @@ import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class FeedModel(
+    @Json(name = "collection") val collection: Collection,
     @Json(name = "stocks") val stocks: Map<String, Double>,
     @Json(name = "currencies") val currencies: Map<String, CurrencyModel>,
     @Json(name = "cryptocurrencies") val cryptoCurrencies: Map<String, Double>
 )
+
+@JsonClass(generateAdapter = true)
+data class Collection(
+    @Json(name = "stock") val stocks: Map<String, Double>,
+    @Json(name = "currency") val currencies: Map<String, CurrencyModel>,
+    @Json(name = "cryptocurrency") val cryptoCurrencies: Map<String, Double>
+)
+
